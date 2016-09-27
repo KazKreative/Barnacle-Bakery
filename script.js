68ab35de60144e9b9930b33672fb125af63f94bf
Jonathan Kaz <contact@redstarfilmsnc.com>
2016-09-27 15:43:19 -0400

    added current count with results

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function OnChange(dropdown)
-{
-    var myindex  = dropdown.selectedIndex
-    var SelValue = dropdown.options[myindex].value
-    
-    top.location.href = baseURL;
-    
-    return true;
-}
+
+var orderStatus = document.getElementById("orderStatus");
+
+var orderButton = document.getElementById("orderButton");
+var lemonCounter = document.getElementById("lemonCounter")
+
+
+document.getElementById("orderButton").addEventListener("click", function(event) {
+	document.getElementById("orderButton").style.display = 'none';
+	console.log('you clicked Lemon')
+	lemonCounter.innerHTML = "1 cookie ordered";
+
+})
